fe0b82b251e1f7826d3e9dbb0355c87d4af90d13
apoleon33 <simondumesny@gmail.com>
2023-02-16 15:24:41 +0100

    contact page

---
+++ src/routes/association/contact/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+	import Phone from './Phone.svelte';
+	import At from './At.svelte';
+	import Facebook from './Facebook.svelte';
+</script>
+
+<div id="wrapper">
+	<h1>Nous contacter</h1>
+	<div id="listeContact">
+		<div class="iconAndContentWrapper">
+			<Phone />
+			<h2>Par téléphone: au ...</h2>
+		</div>
+		<div class="iconAndContentWrapper">
+			<At />
+			<h2>Par mail: ...</h2>
+		</div>
+		<div class="iconAndContentWrapper">
+			<Facebook />
+			<h2>Sur facebook: ...</h2>
+		</div>
+	</div>
+</div>
+
+<style>
+	.iconAndContentWrapper {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		gap: 10px;
+	}
+
+	#wrapper {
+		padding: 1em;
+	}
+</style>
