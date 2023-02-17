dac457c2943754e1f7b6174024b65a34cd442a56
apoleon33 <simondumesny@gmail.com>
2023-02-17 20:24:26 +0100

    fixed bug when going in /animaux/autres

--- src/routes/animaux/[animal]/+page.svelte
+++ src/routes/animaux/[animal]/+page.svelte
@@ -17,18 +17,24 @@
 
 <div id="description">
 	<h1>
-		{#if data.id === 'chats' || data.id === 'chiens'}
-			Nos {data.id} disponibles à l'adoption
+		{#if listeAnimaux.length != 0}
+			{#if data.id === 'chats' || data.id === 'chiens'}
+				Nos {data.id} disponibles à l'adoption
+			{:else}
+				Nos autres animaux disponibles à l'adoption
+			{/if}
 		{:else}
-			Nos autres animaux disponibles à l'adoption
+			<h3>Pas d'animaux à adopter en ce moment!</h3>
 		{/if}
 	</h1>
 </div>
 {#key data}
 	<div id="animalWrapper" in:fade>
-		{#each listeAnimaux as { id, name, link }, i}
-			<Animal {name} lien={link} onclic="/{data.id}/{id}" />
-		{/each}
+		{#if listeAnimaux.length != 0}
+			{#each listeAnimaux as { id, name, link }, i}
+				<Animal {name} lien={link} onclic="/animaux/{data.id}/{id}" />
+			{/each}
+		{/if}
 	</div>
 {/key}
 
