a0459f7415d6f434290ca2773ff9c04650f5f3a6
apoleon33 <simondumesny@gmail.com>
2023-04-08 23:24:16 +0200

    small border radius on the menu

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -11,7 +11,7 @@
 			<div class="dropdown-content">
 				<a href="/" class="text option-menu"> Accueil </a>
 				<a href="/association/contact" class="text option-menu"> Contact</a>
-				<a href="/association/archives" class="text option-menu"> Archives </a>
+				<a href="/association/archives" class="text option-menu last-element"> Archives </a>
 			</div>
 		</div>
 		<div class="dropdown">
@@ -20,7 +20,7 @@
 				<a href="/comment-ca-marche" class="text option-menu"> Comment ça marche ? </a>
 				<a href="/animaux/chats" class="text option-menu"> Chats </a>
 				<a href="/animaux/chiens" class="text option-menu"> Chiens</a>
-				<a href="/animaux/autres" class="text option-menu"> Autres </a>
+				<a href="/animaux/autres" class="text option-menu last-element"> Autres </a>
 			</div>
 		</div>
 
@@ -28,7 +28,9 @@
 			<span> <a href="/aider" class="text">Nous aider </a></span>
 			<div class="dropdown-content" id="dropdown-help">
 				<a href="/aider/don" class="text option-menu"> Faire un don </a>
-				<a href="/aider/famille-accueil" class="text option-menu"> Devenir famille d'accueil</a>
+				<a href="/aider/famille-accueil" class="text option-menu last-element">
+					Devenir famille d'accueil</a
+				>
 			</div>
 		</div>
 	</div>
@@ -127,6 +129,8 @@
 		min-width: 100%;
 		box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
 		z-index: 1;
+
+		border-radius: 0 0 10px 10px;
 	}
 
 	/* Links inside the dropdown */
@@ -150,6 +154,10 @@
 	.option-menu {
 		padding-right: 30em;
 		font-size: 20px;
+	}
+
+	.last-element {
+		border-radius: 0 0 10px 10px;
 	}
 
 	#dropdown-help {
--- src/routes/Summary.svelte
+++ src/routes/Summary.svelte
@@ -4,7 +4,7 @@
 		<div class="dropdown-content">
 			<a href="/" class="text option-menu"> Accueil </a>
 			<a href="/association/contact" class="text option-menu"> Contact</a>
-			<a href="/association/archives" class="text option-menu"> Archives </a>
+			<a href="/association/archives" class="text option-menu last-element"> Archives </a>
 		</div>
 	</div>
 	<div class="dropdown">
@@ -13,7 +13,7 @@
 			<a href="/comment-ca-marche" class="text option-menu"> Comment ça marche ? </a>
 			<a href="/animaux/chats" class="text option-menu"> Chats </a>
 			<a href="/animaux/chiens" class="text option-menu"> Chiens</a>
-			<a href="/animaux/autres" class="text option-menu"> Autres </a>
+			<a href="/animaux/autres" class="text option-menu last-element"> Autres </a>
 		</div>
 	</div>
 
@@ -21,7 +21,9 @@
 		<span> <a href="/aider" class="text">Nous aider </a></span>
 		<div class="dropdown-content" id="dropdown-help">
 			<a href="/aider/don" class="text option-menu"> Faire un don </a>
-			<a href="/aider/famille-accueil" class="text option-menu"> Devenir famille d'accueil</a>
+			<a href="/aider/famille-accueil" class="text option-menu last-element">
+				Devenir famille d'accueil</a
+			>
 		</div>
 	</div>
 </div>
@@ -72,6 +74,8 @@
 		min-width: 100%;
 		box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
 		z-index: 1;
+
+		border-radius: 0 0 10px 10px;
 	}
 	/* Links inside the dropdown */
 	.dropdown-content a {
@@ -94,6 +98,10 @@
 	.option-menu {
 		padding-right: 30em;
 		font-size: 20px;
+	}
+
+	.last-element {
+		border-radius: 0 0 10px 10px;
 	}
 
 	#dropdown-help {
