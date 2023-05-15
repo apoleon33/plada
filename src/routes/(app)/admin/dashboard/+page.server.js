3daeeee53178e78f3edf51fddc257a59a83d7ccc
apoleon33 <simondumesny@gmail.com>
2023-05-15 16:03:10 +0200

    added autres animaux + bunch of other things

--- src/routes/(app)/admin/dashboard/+page.server.js
+++ src/routes/(app)/admin/dashboard/+page.server.js
@@ -9,6 +9,9 @@ export async function load({ params }) {
 	let listImageChiensPromises = [];
 	let listImageChiens = [];
 
+	let listImageAutresPromises = [];
+	let listImageAutres = [];
+
 	for (let i = 0; i < database[0].chats.length; i++) {
 		listImageChatPromises.push(getUrl(database[0].chats[i].link));
 	}
@@ -16,13 +19,20 @@ export async function load({ params }) {
 	for (let i = 0; i < database[0].chiens.length; i++) {
 		listImageChiensPromises.push(getUrl(database[0].chiens[i].link));
 	}
+
+	for (let i = 0; i < database[0].autres.length; i++) {
+		listImageAutresPromises.push(getUrl(database[0].autres[i].link));
+	}
+
 	listImageChat = await Promise.all(listImageChatPromises);
 	listImageChiens = await Promise.all(listImageChiensPromises);
+	listImageAutres = await Promise.all(listImageAutresPromises);
 
 	return {
 		db: database[0],
 		imageChats: listImageChat,
-		imageChiens: listImageChiens
+		imageChiens: listImageChiens,
+		imageAutres: listImageAutres
 	};
 }
 
