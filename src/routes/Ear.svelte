5ce44ca4bafb5e51d7e5043985a71053c2c3f27c
apoleon33 <simondumesny@gmail.com>
2023-08-17 22:24:20 +0200

    better Ear component

--- src/routes/Ear.svelte
+++ src/routes/Ear.svelte
@@ -1,23 +1,30 @@
 <script>
 	import { fly } from 'svelte/transition';
 	export let xIntro = -300;
-	export let width = 10;
+	// export let width = 90;
 	export let color = '--secondary-color';
 </script>
 
-<div
-	id="ear"
-	in:fly={{ x: xIntro, duration: 500 }}
-	style="		
-	border-left: {width}vh solid transparent;
-	border-right: {width}vh solid transparent;
+<!--	style="		
+	border-left: {width}px solid transparent;
+	border-right: {width}px solid transparent;
 
-	border-bottom: {width}vh solid var({color});"
-/>
+	border-bottom: {width}px solid var({color});"-->
+
+<svg viewBox="0 0 100 90" width="90" height="90" in:fly={{ x: xIntro, duration: 500 }} id="ear">
+	<path
+		d="M 20 120
+			 L 60 40
+			 Q 70 20 80 40
+			 L 120 120    
+			 Z"
+		transform="translate(-20,-20)"
+		stroke-width="0"
+	/>
+</svg>
 
 <style>
 	#ear {
-		width: 0;
-		height: 0;
+		fill: var(--secondary-color);
 	}
 </style>
